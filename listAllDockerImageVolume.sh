d5fca44799d9dec7ea9bcfef4b8677c330ee3ad9
Nekrofage <lesanglierdesardennes@gmail.com>
2024-10-03 22:47:28 +0200

    [Add] Podman scripts

--- listAllDockerImageVolume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-podman images
--- removeAllDockerImageVolume.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo podman rm -f $(podman ps -a -q)
-sudo podman rmi -f $(podman images -q)
-sudo podman volume rm $(podman volume ls -qf dangling=true)
