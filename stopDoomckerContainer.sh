9880975a75ada1b629276e27b6e76ade775c287e
Nekrofage <lesanglierdesardennes@gmail.com>
2024-10-05 15:09:38 +0200

    [Update] Scripts name

--- stopDoomckerContainer.sh
+++ stopDoomckerContainer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-podman stop $(podman ps -a -q --filter ancestor=nekrofage/doomcker:latest --format="{{.ID}}")
+podman stop $(podman ps -a -q --filter ancestor=localhost/nekrofage/doomcker:latest --format="{{.ID}}")
